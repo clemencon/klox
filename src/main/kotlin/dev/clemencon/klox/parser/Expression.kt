f7c8636fb9a225a86888b409e64c4635b6e43f49
Julien Clémençon <julien@clemencon.dev>
2025-11-04 06:45:33 +0100

    Introduce expressions
    Add a temporary AST printer.

---
+++ src/main/kotlin/dev/clemencon/klox/parser/Expression.kt
@@ -0,0 +1,39 @@
+package dev.clemencon.klox.parser
+
+import dev.clemencon.klox.scanner.Token
+
+sealed interface Expression
+
+data class Literal(
+    val value: Any?
+) : Expression
+
+data class Unary(
+    val operator: Token,
+    val right: Expression
+) : Expression
+
+data class Binary(
+    val left: Expression,
+    val operator: Token,
+    val right: Expression
+) : Expression
+
+data class Grouping(
+    val expression: Expression
+) : Expression
+
+// The print function will be removed.
+
+fun Expression.printAst() = when (this) {
+    is Binary -> parenthesize(this.operator.lexeme, this.left, this.right)
+    is Grouping -> parenthesize("group", this.expression)
+    is Literal -> if (this.value == null) "nil" else this.value.toString()
+    is Unary -> parenthesize(this.operator.lexeme, this.right)
+}
+
+fun parenthesize(name: String, vararg expression: Expression): String = buildString {
+    append("($name")
+    expression.forEach { append(" ${it.printAst()}") }
+    append(")")
+}
